17fb59524013904a4526e6a331da6eafe03fca40
Tin-Pui <tchan15@gmail.com>
2017-07-18 09:38:12 +0100

    Moved the main.css file from static/built/ to static/ so that data is shown in a table format when using a browser. Added more entities. EmployeeRepository now extends PagingAndSortingRepository instead of CrudRepository to add the ability to divide data into pages and sort them when using the terminal.

--- src/main/java/com/aquaq/tinpui/springdemo/springdemo/EmployeeRepository.java
+++ src/main/java/com/aquaq/tinpui/springdemo/springdemo/EmployeeRepository.java
@@ -1,7 +1,7 @@
 package com.aquaq.tinpui.springdemo.springdemo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 
 }
